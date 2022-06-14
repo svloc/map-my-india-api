2f8c64f3907d7755abcc96c247572c731c2d0b01
svloc <svloc39@gmail.com>
2022-06-14 15:30:50 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { MapServiceService } from './map-service.service';
+
+declare var MapmyIndia: any;
+declare var L: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Map';
+  public map : any;
+  constructor(private mapService: MapServiceService) {
+  }
+  private token: string;
+
+  ngOnInit(): void {
+    this.map = new MapmyIndia.Map('map',
+    {
+        center: [28.04, 78.2],
+        zoom: 12
+    });
+
+    this.mapService.getToken().then((data) => {
+      this.token = data['access_token'];
+      console.log(data);
+      localStorage.setItem('Token',data['access_token'])
+    });
+  }
+
+  auto() {
+    this.mapService.autoSuggest(this.token).then((data) => {
+      console.log(data);
+    });
+  }
+
+  nearby() {
+    this.mapService.nearby(this.token).then((data) => {
+      console.log(data);
+    });
+  }
+
+  geocode(){
+    this.mapService.geocode(this.token).then((data) => {
+      console.log(data);
+    });
+  }
+
+  textsearch(){
+    this.mapService.textsearch(this.token).then((data) => {
+      console.log(data);
+    });
+  }
+
+  distanceMatrix() {
+    // tslint:disable-next-line: max-line-length
+    this.mapService.jsonp('https://apis.mapmyindia.com/advancedmaps/v1/<Rest_Key>/distance_matrix/driving/77.983936,28.255904;77.05993,28.487555;77.15993,28.587555;77.264997,28.554534?sources=0;1&destinations=2;3').then(function(data) {
+        console.log(data);
+    });
+  }
+
+  Rev_geocode() {
+    // tslint:disable-next-line: max-line-length
+    this.mapService.jsonp('https://apis.mapmyindia.com/advancedmaps/v1/<Rest_Key>/rev_geocode?lng=77.22479939&lat=28.66289505').then(function(data) {
+        console.log(data);
+    });
+  }
+
+  route_adv() {
+      // tslint:disable-next-line: max-line-length
+    this.mapService.jsonp('https://apis.mapmyindia.com/advancedmaps/v1/<Rest_Key>/route_adv/driving/77.227434,28.610981;77.212021,28.616679?alternatives=true&&geometries=polyline&overview=full&exclude=&steps=true&region=ind').then(function(data) {
+        console.log(data);
+    });
+  }
+
+  route_eta() {
+      // tslint:disable-next-line: max-line-length
+    this.mapService.jsonp('https://apis.mapmyindia.com/advancedmaps/v1/<Rest_Key>/route_eta/driving/77.227434,28.610981;77.212021,28.616679?alternatives=true&&geometries=polyline&overview=full&exclude=&steps=true&region=ind').then(function(data) {
+        console.log(data);
+    });
+  }
+
+}
---
+++ src/app/map-service.service.ts
@@ -0,0 +1,112 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MapServiceService {
+  constructor(private httpClient: HttpClient) {
+  }
+
+  private grantType: string = "client_credentials"
+  private clientId: string = "33OkryzDZsJLvAvgoVdfqBFPR2OMNMt-i-Zoc2MvOAT5EG4ELSe7Eed27wZLhcF5_b7G4GIGvpQ99uEPTXqeXsmE5ipEKx0D";
+  private clientSecret: string = "lrFxI-iSEg-XLB1HVZCWs6ds8l_GayOWqGqeVExbiV_W7pdAN3QHbaExTb5XzCJwR981Leg6NZL-_1Lk3Tm8brsm8gaClNQUJTYZDxCFq4s=";
+  private securityURL: string = "https://outpost.mapmyindia.com/api/security/oauth/token?grant_type=" + this.grantType + "&client_id=" + this.clientId + "&client_secret=" + this.clientSecret;
+  private autoSuggestURL: string = "https://atlas.mapmyindia.com/api/places/search/json?query=agra&location=28.5454,77.455454&bridge&explain&username=balmukand";
+  private nearbyURL: string = "https://atlas.mapmyindia.com/api/places/nearby/json?explain&richData&username=balmukand&refLocation=28.467470,77.077518&keywords=FINATM";
+  private geocodeURL: string = "https://atlas.mapmyindia.com/api/places/geocode?address=mapmyindia 237 okhla phase 3";
+  private textsearchURL: string = "https://atlas.mapmyindia.com/api/places/textsearch/json?query=okhla phase 3&region=ind";
+
+  getToken() {
+    let promise = new Promise((resolve, reject) => {
+        let apiURL = this.securityURL;
+        this.httpClient.post(apiURL, null)
+          .toPromise()
+          .then(
+          res => { // Success
+              resolve(res);
+            }
+        );
+    });
+    return promise;
+  }
+
+  autoSuggest(token: string) {
+      const _url = this.autoSuggestURL+"&access_token="+token;
+      const promise = new Promise((resolve, reject) => {
+        this.httpClient.get(_url)
+          .toPromise()
+          .then(
+              res => { // Success
+                  // console.log(res);
+                  resolve(res);
+              }
+          );
+      });
+      return promise;
+  }
+
+  nearby(token: string) {
+    const _url = this.nearbyURL+"&access_token="+token;
+    const promise = new Promise((resolve, reject) => {
+      this.httpClient.get(_url)
+        .toPromise()
+        .then(
+            res => { // Success
+                resolve(res);
+            }
+        );
+    });
+    return promise;
+  }
+
+  geocode(token: string) {
+    const _url = this.geocodeURL+"&access_token="+token;
+    const promise = new Promise((resolve, reject) => {
+      this.httpClient.get(_url)
+        .toPromise()
+        .then(
+            res => { // Success
+                resolve(res);
+            }
+        );
+    });
+    return promise;
+  }
+
+  textsearch(token: string) {
+    const _url = this.textsearchURL+"&access_token="+token;
+    const promise = new Promise((resolve, reject) => {
+      this.httpClient.get(_url)
+        .toPromise()
+        .then(
+            res => { // Success
+                resolve(res);
+            }
+        );
+    });
+    return promise;
+  }
+
+
+  jsonp(uri) {
+    return new Promise(function(resolve, reject) {
+      const id = '_' + Math.round(10000 * Math.random());
+        const callbackName = 'jsonp_callback_' + id;
+        window[callbackName] = function(data) {
+            delete window[callbackName];
+            const ele = document.getElementById(id);
+            ele.parentNode.removeChild(ele);
+            resolve(data);
+        };
+        const src = uri + '&callback=' + callbackName;
+        const script = document.createElement('script');
+        script.src = src;
+        script.id = id;
+        script.addEventListener('error', reject);
+        (document.getElementsByTagName('head')[0] || document.body || document.documentElement).appendChild(script);
+    });
+  }
+
+}
